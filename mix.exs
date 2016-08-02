9b40e38bcfd32eca0c6d74440fa975a359bbb2d4
Jeff Weiss <jeff.weiss@puppet.com>
2016-08-02 14:43:33 -0700

    update environments for which mix docs is available

--- mix.exs
+++ mix.exs
@@ -59,8 +59,8 @@ defmodule EctoLdap.Mixfile do
       {:timex, "~> 3.0"},
       {:timex_ecto, "~> 3.0"},
       {:excoveralls, "~> 0.5", only: :test},
-      {:ex_doc, "~> 0.11", only: :docs},
-      {:earmark, "~> 1.0", override: true, only: :docs},
+      {:ex_doc, "~> 0.11", only: [:dev, :docs]},
+      {:earmark, "~> 1.0", override: true, only: [:dev, :docs]},
       {:dialyxir, "~> 0.3", only: :dev},
     ]
   end
